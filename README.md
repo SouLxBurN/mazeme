c3c56ffa1e2ca8b571b6ab179d472f8969f3b801
Tristan Farmer <tristanfarmer@gmail.com>
2022-02-11 02:01:27 -0800

    Collisions, centering, game state refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+# MazeMe
+
+This is a terminal rendered maze game written in Rust.
+
+This is currently a work in progress.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::io::{self, Write};
 use std::sync::mpsc;
 use std::thread;
 
 use console::Term;
+use figlet_rs::FIGfont;
 
 /// Create program that renders a single character in the middle of the screen on launch.
 /// Character is controllable with arrow inputs.
@@ -13,7 +15,8 @@ const SYMBOL: char = '●';
 const ESC: &str = "\x1b";
 const BOARD_SIZE: usize = 25;
 
-const SHADE: char = '░';
+const BORDER: char = '░';
+const WALL: char = '█';
 
 enum Movement {
     UP,
@@ -22,14 +25,80 @@ enum Movement {
     RIGHT,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, Clone)]
+struct GameState {
+    board: [[char; BOARD_SIZE]; BOARD_SIZE],
+    position: Position,
+    win_position: Position,
+    victory: bool,
+}
+
+impl GameState {
+    /// move_position
+    pub fn move_position(&mut self, action: Movement) {
+        let mut new_pos = Position {
+            y: self.position.y,
+            x: self.position.x,
+        };
+
+        match action {
+            Movement::UP => new_pos.y = (self.position.y + self.board.len() - 1) % self.board.len(),
+            Movement::DOWN => new_pos.y = (self.position.y + 1) % self.board.len(),
+            Movement::LEFT => {
+                new_pos.x = (self.position.x + self.board.len() - 1) % self.board.len()
+            }
+            Movement::RIGHT => new_pos.x = (self.position.x + 1) % self.board.len(),
+        }
+
+        if self.is_valid_move(&new_pos) {
+            self.board[self.position.y][self.position.x] = ' ';
+            self.board[new_pos.y][new_pos.x] = SYMBOL;
+            self.position = new_pos;
+
+            if self.is_win_position() {
+                self.victory = true;
+            }
+        }
+    }
+
+    /// is_valid_move
+    /// Accepts a board reference and the destination position.
+    /// Returns true if move is valid, otherwise false.
+    fn is_valid_move(&self, new_position: &Position) -> bool {
+        !self.victory && self.board[new_position.y][new_position.x] == ' '
+    }
+
+    fn is_win_position(&self) -> bool {
+        self.position == self.win_position
+    }
+}
+
 fn main() {
-    let mut board: [[char; BOARD_SIZE]; BOARD_SIZE] = [[' '; BOARD_SIZE]; BOARD_SIZE];
-    let mut position: (usize, usize) = (BOARD_SIZE/2, BOARD_SIZE/2);
-    board[position.0][position.1] = SYMBOL;
+    let mut state = GameState {
+        board: [[' '; BOARD_SIZE]; BOARD_SIZE],
+        position: Position {
+            x: BOARD_SIZE / 2,
+            y: BOARD_SIZE / 2,
+        },
+        win_position: Position { x: 0, y: 0 },
+        victory: false,
+    };
+
+    state.board[state.position.y][state.position.x] = SYMBOL;
+    state.board[state.position.y + 2][state.position.x + 2] = WALL;
+    state.board[state.position.y + 2][state.position.x] = WALL;
+    state.board[state.position.y + 2][state.position.x + 1] = WALL;
+    state.board[state.position.y + 1][state.position.x + 2] = WALL;
 
     let (tx, rx) = mpsc::channel();
-    thread::spawn(|| render(rx));
-    if let Err(e) = tx.send(board.clone()) {
+    thread::spawn(move || render(rx));
+    if let Err(e) = tx.send(state.clone()) {
         panic!("Could not send board state to render {e}");
     }
 
@@ -37,54 +106,75 @@ fn main() {
 
     loop {
         if let Ok(c) = stdout.read_char() {
-            position = match c {
-                'w' => move_position(&mut board, position, Movement::UP),
-                'a' => move_position(&mut board, position, Movement::LEFT),
-                's' => move_position(&mut board, position, Movement::DOWN),
-                'd' => move_position(&mut board, position, Movement::RIGHT),
-                _ => break,
+            match c {
+                'w' => state.move_position(Movement::UP),
+                'a' => state.move_position(Movement::LEFT),
+                's' => state.move_position(Movement::DOWN),
+                'd' => state.move_position(Movement::RIGHT),
+                _ => (),
             };
-            if let Err(e) = tx.send(board.clone()) {
+
+            // Render
+            if let Err(e) = tx.send(state.clone()) {
                 panic!("Could not send board state to render {e}");
             }
         }
     }
 }
 
-/// move_position
-fn move_position(board: &mut [[char; BOARD_SIZE]; BOARD_SIZE], (mut y_pos, mut x_pos): (usize, usize), action: Movement) -> (usize, usize) {
-    board[y_pos][x_pos] = ' ';
-    match action {
-        Movement::UP => y_pos = (y_pos + board.len() - 1) % board.len(),
-        Movement::DOWN => y_pos = (y_pos+1) % board.len(),
-        Movement::LEFT => x_pos = (x_pos + board.len() - 1) % board.len(),
-        Movement::RIGHT => x_pos = (x_pos+1) % board.len(),
-    }
-    board[y_pos][x_pos]= SYMBOL;
-    (y_pos, x_pos)
-}
-
 /// render
-fn render(rx: mpsc::Receiver<[[char; BOARD_SIZE]; BOARD_SIZE]>) {
+fn render(rx: mpsc::Receiver<GameState>) {
+    let (wd, ht) = term_size::dimensions().unwrap_or((BOARD_SIZE + 1, BOARD_SIZE + 1));
+    let (draw_x, draw_y) = ((wd - (3 * BOARD_SIZE + 1)) / 2, (ht - BOARD_SIZE + 1) / 2);
+
     loop {
-        if let Ok(board) = rx.recv() {
-            println!("{ESC}[2J{ESC}[H");
-            for _ in 0..(3*BOARD_SIZE+2) {
-                print!("{SHADE}");
+        if let Ok(state) = rx.recv() {
+            print!("{ESC}[2J{ESC}[{draw_y};{draw_x}H");
+
+            // Draw Top Border
+            for _ in 0..(3 * BOARD_SIZE + 2) {
+                print!("{BORDER}");
             }
-            println!("{ESC}[E{ESC}[A");
-            for row in board.iter() {
-                print!("{SHADE}");
+
+            print!("{ESC}[E{ESC}[{draw_x}G");
+            // Draw each row
+            for row in state.board.iter() {
+                // Left Border
+                print!("{BORDER}");
                 for v in row.iter() {
-                    print!(" {} ", v);
+                    match *v {
+                        SYMBOL => print!("◀◆▶"),
+                        _ => print!("{v}{v}{v}"),
+                    }
                 }
-                print!("{SHADE}");
-                println!("{ESC}[E{ESC}[A");
+                // Right Border
+                print!("{BORDER}\n");
+                print!("{ESC}[{draw_x}G");
             }
-            for _ in 0..(3*BOARD_SIZE+2) {
-                print!("{SHADE}");
+
+            // Draw Bottom Border
+            for _ in 0..(3 * BOARD_SIZE + 2) {
+                print!("{BORDER}");
             }
-            println!("");
+
+            if state.victory {
+                let ffont = FIGfont::standand().unwrap();
+                if let Some(msg) = ffont.convert("You Win!") {
+                    let mut m_w = msg.to_string().lines().map(|s| s.len()).max().unwrap_or(1);
+                    let mut m_h = msg.height as usize;
+                    if m_w > wd || m_h > ht {
+                        m_w = 0;
+                        m_h = 0;
+                    }
+                    let midpoint = ((wd - m_w) / 2, (ht - m_h) / 2);
+                    for (i, l) in msg.to_string().lines().enumerate() {
+                        print!("{ESC}[{ht};{w}H", w = midpoint.0, ht = midpoint.1 + i);
+                        print!("{}", l);
+                    }
+                }
+            }
+
+            io::stdout().flush().unwrap();
         }
     }
 }
